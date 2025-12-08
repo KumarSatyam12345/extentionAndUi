9b820434a02bb22fd372c588984bc6c2ff32bd05
Admin <sk95232696@gmail.com>
2025-12-08 16:13:59 +0530

    initial commit

--- chrome-extension/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.type === "OPEN_URL") {
-    const finalURL = msg.payload.includes("?")
-      ? msg.payload + "&openedByExtension=true"
-      : msg.payload + "?openedByExtension=true";
-
-    chrome.tabs.create({ url: finalURL });
-  }
-});
